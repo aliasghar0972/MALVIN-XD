1e7c4bce9b3de3218257af15e0741ef89735fe92
aliasghar0972 <aliasghar1234584@gmail.com>
2025-08-17 22:29:06 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~oncWnRJA#ORKyxeG87cUpUNxw0paArjjS0MUX8BBaWClnhQ1YmC8",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
